f4f92e5075525f37b9fb759072015a4a33be3e1f
kari0036 <kgebski68@gmail.com>
2020-10-10 21:54:07 +0200

    add hand

---
+++ Next Please Game/ręka.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+
+enum STATE{
+	IDLE,
+	GRAB
+}
+
+onready var sprite = $sprite
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+func grab_state():
+	sprite.frame = 1
+	
+func idle_state():
+	sprite.frame = 0
+
+
+
+
+var current_state
+func _physics_process(delta):
+	position = lerp(get_global_mouse_position(),position,0.6)
+
+	if (Input.is_action_pressed("grab")): current_state = STATE.GRAB
+	else: current_state = STATE.IDLE
+
+	match current_state:
+		STATE.IDLE:
+			idle_state()
+		STATE.GRAB:
+			grab_state()
