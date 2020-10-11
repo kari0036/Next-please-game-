f0819261221d5a2bc271e0576ba765f4f56c20e6
Jacenty <jacekjarosz55@gmail.com>
2020-10-11 14:38:40 +0200

    ukończone przenoszenie przedmiotów (yay)

--- Next Please Game/scripts/ręka.gd
+++ Next Please Game/scripts/ręka.gd
@@ -12,19 +12,32 @@ onready var sprite = $sprite
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
+
 var carried_item
+var saved_pos = Vector2.ZERO
+
 func grab_state():
-	if (!carried_item):
-		var items = get_overlapping_areas()
-		if items:
-			carried_item = items[0]
+	if !carried_item:
+		for body in  get_overlapping_bodies():
+			if body.is_in_group("items"):
+				carried_item = body
+				saved_pos = carried_item.position
+				break
 	else:
 		carried_item.position = position
-	
+		
 	
 	sprite.frame = 1
 func idle_state():
-	carried_item = null
+	var on_holder = false
+	if carried_item:
+		
+		for body in get_overlapping_bodies():
+				if body.is_in_group("item_holder"):
+					on_holder = true
+		if !on_holder:
+			carried_item.position = saved_pos
+		carried_item = null
 	sprite.frame = 0
 
 
