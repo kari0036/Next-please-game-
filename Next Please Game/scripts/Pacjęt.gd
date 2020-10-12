85e30f0b8c43f5c6f7116cf4bdae0101e4d0a03e
kari0036 <kgebski68@gmail.com>
2020-10-12 10:27:14 +0200

    Next please move work

---
+++ Next Please Game/scripts/Pacjęt.gd
@@ -0,0 +1,45 @@
+extends KinematicBody2D
+
+var velocity = Vector2()
+var room = Vector2(141,123)
+var outOfRoom = Vector2(400,123)
+
+var heMove = false
+var firstPatient = true
+var goToRoom = false
+
+func nextPlease():
+	if !heMove:
+		heMove = true
+			
+func _physics_process(delta):
+	if heMove && firstPatient:
+		goTo(room)
+	else: if heMove:
+		changePatient()
+	
+func goTo(target):
+	velocity = position.direction_to(target) * 100
+	if position.distance_to(target) > 5:
+		velocity = move_and_slide(velocity)
+	else:
+		heMove = false
+		firstPatient = false
+		
+		
+func changePatient():
+	if !goToRoom:
+		velocity = position.direction_to(outOfRoom) * 100
+		if position.distance_to(outOfRoom) > 5:
+			velocity = move_and_slide(velocity)
+		else: 
+			#TODO zmmień wygląd postaci
+			goToRoom = true
+	else:
+		velocity = position.direction_to(room) * 100
+		if position.distance_to(room) > 5:
+			velocity = move_and_slide(velocity)
+		else:
+			goToRoom = false
+			heMove = false
+	
